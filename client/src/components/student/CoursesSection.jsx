28169fa57628d7a6ceace3015de223c0d1cc3b9b
Marcos Jova <marcosjova3@gmail.com>
2025-03-04 04:21:26 +0530

    UPDATE

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -24,7 +24,7 @@ const CoursesSection = () => {
 			</p>
 
 			<div className="grid  grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4">
-				{allCourses.slice(0,4).map((course,index) => <CourseCard key={index} course={course}/> )}
+				{allCourses.slice(0,12).map((course,index) => <CourseCard key={index} course={course}/> )}
 			</div>
 			<Link to={"/course-list"} onClick={() => scrollTo(0, 0)} className="text-gray-500 border border-gray-500/30 px-10 py-3 rounded">
 				Ver mais cursos
