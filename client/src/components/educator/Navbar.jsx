d490bc3f385961b788e1834d0cbda6f2c2c3d8cf
Marcos Jova <marcosjova3@gmail.com>
2025-03-20 01:55:22 +0530

    updates

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -9,7 +9,8 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3'>
       <Link to='/'>
-      <img src={assets.logo} alt="logo" className='w-28 lg:w-32' />
+      {/* <img src={assets.logo} alt="logo" className='w-28 lg:w-32' /> */}
+      <img src={assets.logo} alt="logo"  />
       </Link>
 
       <div className='flex items-center gap-5 text-gray-500 relative'>
